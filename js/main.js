7d00ec0e41a3dee51f86fcf92e338e5c4cc43a4e
natasha-gri <natashagri.dw@gmail.com>
2022-09-29 17:03:41 +0500

    переверстка в основном меню под WP - 29.09-17:00

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+function fontsize() {
+    let block = document.querySelector('.products-cols-col');
+    console.log(block);
+    let text = document.querySelector('.text-name-card');
+    let w = block.offsetWidth;
+    text.style.fontSize = w/10 + "px";
+    // text.style.top = w/38 + "px";
+    // text.style.left = w/9 + "px";
+}
+window.onload = fontsize;
+window.onresize = fontsize;  
