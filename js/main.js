d8c27b1cc273421e3a0af43dafd6a2dc3280d697
natasha-gri <natashagri.dw@gmail.com>
2022-09-30 17:46:57 +0500

    настройка слайдеров - 30.09-18:00

--- js/main.js
+++ js/main.js
@@ -11,3 +11,115 @@ function fontsize() {
 }
 window.onload = fontsize;
 window.onresize = fontsize;  
+
+// Cлайдер обложки
+// Инициализируем Swiper
+const swipercover = new Swiper('.slider-cover', {
+    // Optional parameters
+    loop: true,
+      // Автопрокрутка
+      autoplay: {
+      delay: 5000,
+       
+    },
+    // Скорость
+    speed: 2000,
+    // Эффект
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },      
+});
+
+const swiperright = new Swiper('.slider-right', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        // Автопрокрутка
+        autoplay: {
+        delay: 5000,
+         
+        },
+        // Скорость
+        speed: 1000,
+      
+        // Стрелки
+        navigation: {
+            nextEl: '.swiper-button-next',
+            
+        },
+});
+
+const swiperProduct = new Swiper('.slider-product', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    // Автопрокрутка
+    autoplay: {
+    delay: 5000,
+     
+    },
+    // Скорость
+    speed: 1000,
+    
+    // Пагинация
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+        
+    },
+
+    // Стрелки
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+const swiperProcessMini = new Swiper('.slider-process-mini', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 5,
+    
+    
+});
+
+const swiperprocess = new Swiper('.slider-process', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    // Автопрокрутка
+    autoplay: {
+    delay: 2000,     
+    },
+    // Скорость
+    speed: 1000,
+    // Стрелки
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    // trumbs: {
+    //     swiper: {
+    //         el: '.slider-process-mini',
+    //         slidesPerView: 5,
+    //     }
+    // },
+    trumbs: {
+        swiper: swiperProcessMini
+    },
+
+});
+
+// const swiperProcessMini = new Swiper('.slider-process-mini', {
+//     // Optional parameters
+//     direction: 'horizontal',
+//     loop: true,
+//     slidesPerView: 5,
+    
+    
+// });
