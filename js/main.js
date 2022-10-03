0b7287a21bf4ea176a92e0fb50aa830afa49edd4
natasha-gri <natashagri.dw@gmail.com>
2022-10-03 17:58:03 +0500

    меню для подменю, верстка - 3.10-18:00

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
 'use strict';
 
+// меню
+let isMobile = {
+	Android: function() {return navigator.userAgent.match(/Android/i);},
+	BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},
+	iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
+	Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
+	Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
+	any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}
+};
+		let body=document.querySelector('body');
+if(isMobile.any()){
+		body.classList.add('touch');
+		let arrow=document.querySelectorAll('.arrow');
+	for(i=0; i<arrow.length; i++){
+			let thisLink=arrow[i].previousElementSibling;
+			let subMenu=arrow[i].nextElementSibling;
+			let thisArrow=arrow[i];
+
+			thisLink.classList.add('parent');
+		arrow[i].addEventListener('click', function(){
+			subMenu.classList.toggle('open');
+			thisArrow.classList.toggle('active');
+		});
+	}
+}else{
+	body.classList.add('mouse');
+}
+
 // Адаптивный заголовочный шрифт для карточек продукта
 function fontsize() {
     let block = document.querySelector('.products-cols-col');
@@ -13,8 +41,7 @@ function fontsize() {
 window.onload = fontsize;
 window.onresize = fontsize;  
 
-// Cлайдер обложки
-// Инициализируем Swiper
+// слайдер обложки
 const swipercover = new Swiper('.slider-cover', {
     // Optional parameters
     loop: true,
@@ -30,7 +57,7 @@ const swipercover = new Swiper('.slider-cover', {
       crossFade: true
     },      
 });
-
+// слайдер обложки с права
 const swiperright = new Swiper('.slider-right', {
         // Optional parameters
         direction: 'horizontal',
@@ -49,7 +76,7 @@ const swiperright = new Swiper('.slider-right', {
             
         },
 });
-
+// слайдер шапки странциы продукта
 const swiperProduct = new Swiper('.slider-product', {
     // Optional parameters
     direction: 'horizontal',
@@ -76,40 +103,7 @@ const swiperProduct = new Swiper('.slider-product', {
         prevEl: '.swiper-button-prev',
     },
 });
-
-// процесс
-const swiperProcessMini = new Swiper('.slider-process-mini', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 5,
-    
-    
-});
-
-const swiperprocess = new Swiper('.slider-process', {
-    // Optional parameters
-    direction: 'horizontal',
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    // Автопрокрутка
-    // autoplay: {
-    // delay: 2000,     
-    // },
-    // Скорость
-    speed: 1000,
-    // Стрелки
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    trumbs: {
-        swiper: swiperProcessMini
-    },
-});
-// пример
-
+// слайдеры процесса
 var sliderprocmini = new Swiper(".slider-proc-mini", {
   // loop: true,
   spaceBetween: 10,
