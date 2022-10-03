cc1a16326b11d39bc034d23946876d3c05b4623c
natasha-gri <natashagri.dw@gmail.com>
2022-10-03 12:51:16 +0500

    Верстка карточек новостей - 3.10-13:00

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 'use strict';
 
+// Адаптивный заголовочный шрифт для карточек продукта
 function fontsize() {
     let block = document.querySelector('.products-cols-col');
     console.log(block);
     let text = document.querySelector('.text-name-card');
-    let w = block.offsetWidth;
-    text.style.fontSize = w/10 + "px";
-    // text.style.top = w/38 + "px";
-    // text.style.left = w/9 + "px";
+    let w = blocks[0].offsetWidth // сюда запишем все ширину у каждого блока
+    texts.forEach(text => {
+        text.style.fontSize = w/100 + "px"
+    })
 }
 window.onload = fontsize;
 window.onresize = fontsize;  
@@ -130,4 +131,5 @@ var sliderprocmain = new Swiper(".slider-proc-main", {
     swiper: sliderprocmini,
   },
 });
+
 
